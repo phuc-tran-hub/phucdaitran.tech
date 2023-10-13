66eafdc8e2a01b7b9bb4477c055cb767e3a07b38
Phuc Tran <phuc.d.tran.24@dartmouth.edu>
2023-10-12 20:45:36 -0400

    basic quiz done!

--- js/quiz.js
+++ js/quiz.js
@@ -11,6 +11,18 @@ const pictures = new Map([
     [3, ["Grimy.jpg", "Beatkill.jpg", "Soulful.jpg", "RedBull.jpg"]],
     [4, ["WinRedBull.jpg", "Impresshaters.jpg", "Silencehaters.jpg", "Formyself.jpg"]]]);
 
+const bboy_pictures = new Map([
+    ["Tiro", "tiro.jpg"],
+    ["Wigor", "wigor.jpg"],
+    ["PhilWizard", "phil.jpg"],
+    ["Hong10", "hong10.jpg"],
+    ["Issin", "issin.jpg"],
+    ["Allef", "allef.jpg"],
+    ["KidKaram", "kidkaram.jpg"],
+    ["X-Rain", "xrain.jpg"],
+    ["Kastrito", "kastrito.jpg"],
+    ["Jeffro", "jeffro.jpg"]]);
+
 const questions = new Map([
     [0, "What's your favorite element of breaking?"],
     [1, "What's the most important trait of your style?"],
@@ -23,23 +35,23 @@ const answers = new Map([
     [1, ["Dynamic", "Explosive", "Character", "Musicality"]],
     [2, ["Asia", "Europe", "South America", "North America"]],
     [3, ["Grimy", "Beatkill", "Soulful", "Red Bull"]],
-    [4, ["Win RedBull", "Impress the Crowd", "Silent the Haters", "For Myself"]]]);
+    [4, ["Win Red Bull BC One", "Impress the Crowd", "Silent the Haters", "For Myself"]]]);
 
 const bboys = new Map([
     ["Tiro", [1, 0, 1, 2, 3]],
     ["Wigor", [3, 1, 1, 0, 3]],
     ["PhilWizard", [2, 2, 3, 1, 1]],
     ["Hong10", [2, 0, 0, 3, 0]],
-    ["Issin", [3, 1, 0, 1, 0],
+    ["Issin", [3, 1, 0, 1, 0]],
     ["Allef", [0, 2, 2, 2, 1]],
     ["KidKaram", [3, 1, 1, 1, 0]],
     ["X-Rain", [3, 1, 0, 1, 2]],
     ["Kastrito", [3, 1, 3, 3, 2]],
-    ["Jeffro", [0, 2, 3, 2, 3]]]]);
+    ["Jeffro", [0, 2, 3, 2, 3]]]);
 
-const user_inputs = [];
 // Updates
 var panelIndex = 0;
+var user_inputs = [];
 
 // Callbacks
 document.getElementById("submit").onclick = function(){
@@ -81,7 +93,12 @@ function checkSubmit(){
     panelIndex = panelIndex % (pictures.size);
     if (panelIndex == 0){
         panelIndex = 0;
-        alert("You are ..");
+        bboy = calculateWinner();
+        displayWinner(bboy);
+        restoreDatastructures();
+    }
+    else{
+        hideWinner();
     }
 
     updateQuestion(panelIndex);
@@ -89,9 +106,56 @@ function checkSubmit(){
 }
 
 
-function calculateWinner(){
-
+function displayWinner(bboy){
+    document.getElementById("final_result").style.display = "block";
+    document.getElementById("bboyname").innerHTML = "You are .. B-Boy " + bboy;
+    document.getElementById("result_img").src = pictures_path + bboy_pictures.get(bboy);
 }
+
+
+function hideWinner(){
+    document.getElementById("final_result").style.display = "none";
+}
+
+function restoreDatastructures(){
+    user_inputs = [];
+}
+
+function calculateWinner(){
+    let counter = new Map([
+        ["Tiro", 0],
+        ["Wigor", 0],
+        ["PhilWizard", 0],
+        ["Hong10", 0],
+        ["Issin", 0],
+        ["Allef", 0],
+        ["KidKaram", 0],
+        ["X-Rain", 0],
+        ["Kastrito", 0],
+        ["Jeffro", 0]]);
+    for(var i = 0; i < user_inputs.length; i++){
+        for(let [key, value] of bboys){
+            bboy_characteristics = value;
+            if (user_inputs[i] == bboy_characteristics[i]){
+                counter.set(key, counter.get(key) + 1);
+            }
+        }
+    }
+
+
+    let max = 0;
+    let maxKey = "";
+    
+    for(let [char, count] of counter){
+        if(count > max){
+            max = count;
+            maxKey = char;
+        }
+    }
+    
+    return maxKey;
+}
+
 
 function updateQuestion(questionIndex){
     document.getElementById("quiz_question").innerHTML = questions.get(questionIndex);
